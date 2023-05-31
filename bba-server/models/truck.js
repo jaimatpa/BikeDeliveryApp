871e07c7f605a5f3370e054e97c654d4676d1750
aw <awr417h@pm.me>
2023-06-01 00:01:25 +0500

    fixed truck resources

--- bba-server/models/truck.js
+++ bba-server/models/truck.js
@@ -14,10 +14,36 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Truck.init({
-    notes: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    notes: DataTypes.STRING,
+    LicensePlate: DataTypes.STRING,
+    TruckName: DataTypes.STRING,
+    MaxCapacity: DataTypes.STRING,
+    HHI_Resort: DataTypes.STRING,
+    Ocean_1: DataTypes.STRING,
+    PD_Pass: DataTypes.STRING,
+    SP_Pass: DataTypes.STRING,
+    SY_Pass: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
   }, {
     sequelize,
     modelName: 'Truck',
+    timestamps: true,
   });
+
+  Truck.beforeCreate((truck, _) => {
+    truck.createdAt = new Date();
+    truck.updatedAt = new Date();
+  });
+
+  Truck.beforeUpdate((truck, _) => {
+    truck.updatedAt = new Date();
+  });
+
   return Truck;
 };
