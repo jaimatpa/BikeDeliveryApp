2ec491201af63a5d091894c753448f71743fa522
Jairo <jairo.pauluz.matos@gmail.com>
2024-03-31 17:06:37 -0700

    checking for cb for null

--- bba-server/api/unprotected/user/resend.js
+++ bba-server/api/unprotected/user/resend.js
@@ -13,8 +13,10 @@ const request = require('request');
 
 function urlExists(url, cb) {
     request({ url: url, method: 'HEAD' }, function(err, res) {
-        if (err) return cb(null, false);
-        cb(null, /4\d\d/.test(res.statusCode) === false);
+        if(cb) {
+          if (err) return cb(null, false);
+          cb(null, /4\d\d/.test(res.statusCode) === false);
+        }
     });
 }
 
