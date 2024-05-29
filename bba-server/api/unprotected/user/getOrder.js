2efdbb01a2a98c319af6d9f1fd5f6fb0b10646da
Jairo <jairo.pauluz.matos@gmail.com>
2024-05-29 12:08:07 -0400

    Scan barcode working

--- bba-server/api/unprotected/user/getOrder.js
+++ bba-server/api/unprotected/user/getOrder.js
@@ -5,38 +5,60 @@ const moment = require("moment");
 
 const models = require("./../../../models");
 const { RecordingSettingsContext } = require("twilio/lib/rest/video/v1/recordingSettings");
+const DeliveryOrderQuery = require("../../../translation/DeliveryOrderQuery");
 
 router.post("/", async (req, res) => {
-    const deliveryOrder = await models.DeliveryOrders.findOne(
-        {
-            where:
-            {
-                orderid:req.query.orderid
-            }
-        });
-        if (deliveryOrder) {
-            return 1;
-        } else {
-            return 0;
-        }
+    // const deliveryOrder = await models.DeliveryOrders.findOne(
+    //     {
+    //         where:
+    //         {
+    //             orderid:req.query.orderid
+    //         }
+    //     });
+
+    const whereConditions = {
+        and: {
+            orderid:req.query.orderid
+        },
+    }
+    const query = DeliveryOrderQuery.translateDeliveryOrder(whereConditions);
+    let data = await models.sequelize.query(query, {
+        type: models.sequelize.QueryTypes.SELECT
+    });
+    let deliveryOrder = data[0];
+    if (deliveryOrder) {
+        return 1;
+    } else {
+        return 0;
+    }
 });
 
 router.get("/", async (req, res) => {
-    const deliveryOrder = await models.DeliveryOrders.findOne(
-        {
-            where:
-            {
-                orderid:req.query.orderid
-            }
-        });
-        console.log(deliveryOrder);
-        if (deliveryOrder) {
-            console.log("IN GET SUCCESS");
-            return res.send('1');
-        } else {
-            console.log("IN GET FAILURE");
-            return res.send('0');
-        }
+    // const deliveryOrder = await models.DeliveryOrders.findOne(
+    //     {
+    //         where:
+    //         {
+    //             orderid:req.query.orderid
+    //         }
+    //     });
+    const whereConditions = {
+        and: {
+            orderid:req.query.orderid
+        },
+    }
+    const query = DeliveryOrderQuery.translateDeliveryOrder(whereConditions);
+    let data = await models.sequelize.query(query, {
+        type: models.sequelize.QueryTypes.SELECT
+    });
+    let deliveryOrder = data[0];
+    console.log(deliveryOrder);
+    if (deliveryOrder) {
+        console.log("IN GET SUCCESS");
+        return res.send('1');
+    } else {
+        console.log("IN GET FAILURE");
+        return res.send('0');
+    }
 });
 
 module.exports = router;
